adeabb9cc5a82385da9e7e3692ca91f3545cbd31
TheSpanishBread <61657019+TheSpanishBread@users.noreply.github.com>
2025-04-23 14:30:14 +0800

    Added new branch

--- script.js
+++ script.js
@@ -1,17 +1,79 @@
+//initialize starting scores
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = "";
+let computerChoice = "";
+let gameover = false;
+///hide table unless game is started
+const table = document.querySelector("#table");
+table.style.display = "none";
+const confirmation = document.querySelector("#confirmation");
+const start = document.querySelector("#start");
+const hscore = document.querySelector("#human-score");
+const cscore = document.querySelector("#computer-score");
+const choice = document.querySelector("#choice");
+const announcement = document.querySelector("#announcement");
 
-function getHumanChoice(){
-    let choice1 = prompt("Rock, Paper, Scissors");
-    choice1 = choice1.toLowerCase();
-    return choice1;
-}
+
+start.addEventListener("click", function(){
+
+    ///display hide unhide
+    table.style.display = "flex";
+    confirmation.style.display = "none";
+    //display scores
+    hscore.textContent = humanScore;
+    cscore.textContent = computerScore;
+    announcement.textContent = "START!";
+
+    choice.addEventListener("click", (e) =>{
+
+        let target = e.target;
+        switch (target.id) {
+            case "rock":
+                humanChoice = "rock";
+                break;
+            case "paper":
+                humanChoice = "paper";
+            break;
+            case "scissors":
+                humanChoice = "scissors";
+            break;
+        
+            default:
+                break;
+        }
+        let computerChoice = getComputerChoice();
+        play(humanChoice, computerChoice);
+        ///update scores
+        hscore.textContent = humanScore;
+        cscore.textContent = computerScore;
+        ////*Check who wins
+        if (humanScore == 5){
+            announcement.textContent = "Human Wins! No AI Take over in the near future! ";
+            e.stopPropagation();
+            humanScore = 0;
+            computerScore = 0;
+        
+        }else if(computerScore == 5){ 
+            announcement.textContent = "Computer Wins! AI has taken over the world! ";
+            e.stopPropagation();
+            humanScore = 0;
+            computerScore = 0;
+        }
+
+    });
+
+    
+});
+
+
+///////functions
+
 
 
 function getComputerChoice(){
     let choice2 = Math.floor(Math.random() * 3);
-     console.log(choice2);
-     
+        
     switch (choice2) {
         case 0:
             choice2 = "rock" ;
@@ -30,46 +92,27 @@ function getComputerChoice(){
             break;
     }
 }
+
 function play(humanChoice, computerChoice){
     if (humanChoice == computerChoice){
-        alert("Tie!");
+        announcement.textContent = "TIE";
     }   else if(humanChoice == "rock" && computerChoice =="paper"){
-        alert("Computer Wins! +1 ");
+        announcement.textContent = "Computer Wins! +1 ";
         computerScore = computerScore + 1 ;
     }   else if(humanChoice == "rock" && computerChoice =="scissors"){
-        alert("Human Wins! +1 ");
+        announcement.textContent = "Human Wins! +1 ";
         humanScore = humanScore + 1 ;
     }   else if(humanChoice == "paper" && computerChoice =="rock"){
-        alert("Human Wins! +1 ");
+        announcement.textContent = "Human Wins! +1 ";
         humanScore = humanScore + 1 ;
     }   else if(humanChoice == "paper" && computerChoice =="scissors"){
-        alert("Computer Wins! +1 ");
+        announcement.textContent = "Computer Wins! +1 ";
         computerScore = computerScore + 1 ;
     }   else if(humanChoice == "scissors" && computerChoice =="rock"){
-        alert("Computer Wins! +1 ");
+        announcement.textContent = "Computer Wins! +1 ";
         computerScore = computerScore + 1 ;
     }   else if(humanChoice == "scissors" && computerChoice =="paper"){
-        alert("Human Wins! +1 ");
+        announcement.textContent = "Human Wins! +1 ";
         humanScore = humanScore + 1 ;
     }
     }
-
-
-while (humanScore != 5 && computerScore != 5) {
-    
-let humanChoice = getHumanChoice();
-let computerChoice = getComputerChoice();
-
-play(humanChoice, computerChoice);
-alert("Score: Human: " + humanScore + "       Computer: " + computerScore + "       Next Round!");
-
-}
-
-
-
-if (humanScore>computerScore){
-    alert("Human Wins!")
-
-}else{
-    alert("Computer Wins!")
-}
